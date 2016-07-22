ab2782b773ed7f5f27814ccc1ed2cef554e8f56d
Moonandeye <MoonAndEye@users.noreply.github.com>
2016-07-22 16:54:25 +0900

    不用存檔, 抓出資料即可, data 就是資料

--- parseTpParking.py
+++ parseTpParking.py
@@ -7,11 +7,22 @@ import urllib.request
 import datetime
 import json
 import pandas as pd
-import gzip
+import gzip, zlib
 
 
 file_name = "test.gz"
 
 url = 'http://data.taipei/tcmsv/allavailable'
 
-urllib.request.urlretrieve(url, file_name)
+#urllib.request.urlretrieve(url, file_name)
+response = urllib.request.urlopen(url)
+data = response.read()      # a `bytes` object
+data = gzip.decompress(data)
+
+"""
+with open(data, "r") as f:
+    text = f.read()
+    text = text.decode('utf-8')
+"""
+
+print(data)
