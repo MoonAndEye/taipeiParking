ac077182d1f8c77ad735dd45013f0856157dd2eb
Moonandeye <MoonAndEye@users.noreply.github.com>
2016-07-22 17:29:35 +0900

    整理好了

--- parseTpParking.py
+++ parseTpParking.py
@@ -17,11 +17,32 @@ url = 'http://data.taipei/tcmsv/allavailable'
 #urllib.request.urlretrieve(url, file_name)
 response = urllib.request.urlopen(url)
 data = response.read()      # a `bytes` object
-data = gzip.decompress(data)
-data = data.decode("big5")
+data = gzip.decompress(data) # data在這一步還是 byte
+data = data.decode("big5") #到這一步才能把 byte 轉成 string
 
 #print(data)
 
 text1 = json.loads(data)
 
 result = text1["data"]["park"]
+
+b4csv = pd.DataFrame(result)
+
+b4csv["availablecar"]  = b4csv["availablecar"].astype(int)
+
+b4csv = b4csv[b4csv.availablecar > 0]
+
+b4csv = pd.DataFrame.reset_index(b4csv)
+
+b4csv = b4csv.drop("index", 1)
+"""
+aftcsv = pd.DataFrame.to_csv(b4csv)
+
+csv_file = open(file_path + str(d0)+'.csv', 'w', encoding = 'utf-8' )
+
+csv_file.write(aftcsv)
+
+csv_file.close()
+
+print ('The ' + str(d0) + ' is done')
+"""
