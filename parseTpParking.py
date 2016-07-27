9481ac4296cea750fbbc3abe81fe5180fb747475
Moonandeye <atimis19@gmail.com>
2016-07-27 23:04:37 +0900

    算是完成了

--- parseTpParking.py
+++ parseTpParking.py
@@ -9,6 +9,10 @@ import json
 import pandas as pd
 import gzip, zlib
 
+d0 = datetime.datetime.now() #d0 是今天
+d0 = d0.strftime("%Y-%m-%d_%H%M%S")
+
+file_path = 'C:/1save/TPcity/'
 
 file_name = "test.gz"
 
@@ -30,11 +34,27 @@ b4csv = pd.DataFrame(result)
 
 b4csv["availablecar"]  = b4csv["availablecar"].astype(int)
 
-b4csv = b4csv[b4csv.availablecar > 0]
+b4csv = b4csv[b4csv.availablecar != -9]
+b4csv = b4csv[b4csv.availablecar != 9999]
+
 
 b4csv = pd.DataFrame.reset_index(b4csv)
 
 b4csv = b4csv.drop("index", 1)
+
+aftcsv = pd.DataFrame.to_csv(b4csv)
+
+
+
+csv_file = open(file_path + str(d0)+'.csv', 'w', encoding = 'utf-8' )
+
+csv_file.write(aftcsv)
+
+csv_file.close()
+
+print ('The ' + str(d0) + ' is done')
+
+
 """
 aftcsv = pd.DataFrame.to_csv(b4csv)
 
